1130f83d76cc7d91dba7b1de67239fc4e37d8e9e
Nikolai <gorbachev.nm@gmail.com>
2026-04-08 02:54:25 +0200

    project init for cynth

---
+++ build/CMakeFiles/cynth.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/nikolay/projects/cynth/src/audio/engine.cpp" "CMakeFiles/cynth.dir/src/audio/engine.cpp.o" "gcc" "CMakeFiles/cynth.dir/src/audio/engine.cpp.o.d"
+  "/Users/nikolay/projects/cynth/src/main.cpp" "CMakeFiles/cynth.dir/src/main.cpp.o" "gcc" "CMakeFiles/cynth.dir/src/main.cpp.o.d"
+  "/Users/nikolay/projects/cynth/src/synth/instruments/piano.cpp" "CMakeFiles/cynth.dir/src/synth/instruments/piano.cpp.o" "gcc" "CMakeFiles/cynth.dir/src/synth/instruments/piano.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/cynth.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cynth.dir/src/audio/engine.cpp.o"
+  "CMakeFiles/cynth.dir/src/audio/engine.cpp.o.d"
+  "CMakeFiles/cynth.dir/src/main.cpp.o"
+  "CMakeFiles/cynth.dir/src/main.cpp.o.d"
+  "CMakeFiles/cynth.dir/src/synth/instruments/piano.cpp.o"
+  "CMakeFiles/cynth.dir/src/synth/instruments/piano.cpp.o.d"
+  "cynth"
+  "cynth.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cynth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
